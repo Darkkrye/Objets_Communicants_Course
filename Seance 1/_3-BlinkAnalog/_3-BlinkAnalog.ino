f5cb83fa6a4374b5291a5e6afe4d5e54faa600b5
Darkkrye <pierre.boudon.27@gmail.com>
2018-03-27 10:59:03 +0200

    Added Seance 1 et README files

---
+++ Seance 1/_3-BlinkAnalog/_3-BlinkAnalog.ino
@@ -0,0 +1,18 @@
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+
+void loop() {
+    // Clignotement de la led : 
+    // La valeur 16 équivaut à la constante LOW du digitaleWrite()
+    // Idem pour 255 avec High
+    analogWrite(LED_BUILTIN, 16);   
+    delay(1000);
+    analogWrite(LED_BUILTIN, 255);   
+    delay(1000);
+    
+         
+}
---
+++ Seance 1/_4-Communication/_4-Communication_Receiver/_4-Communication_Receiver.ino
@@ -0,0 +1,18 @@
+String str; 
+char c;
+
+void setup() {
+  Serial1.begin(9600);
+  Serial.begin(9600);
+}
+
+void loop() {
+  str = "";
+  for(int i=0; i<10;i++){
+    c = Serial1.read(); 
+    str += c;
+    
+  }
+  Serial.println(str);
+  delay(100);
+}
