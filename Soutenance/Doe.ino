bd22f925b2332c243cb1ab22975e19a4eee25cd6
Darkkrye <Darkkrye@users.noreply.github.com>
2018-03-30 11:20:35 +0200

    Update Doe.ino

--- Soutenance/Doe.ino
+++ Soutenance/Doe.ino
@@ -36,140 +36,151 @@ int imageState = FORWARD_STATE;
 void setup()
 {
 
-  pinMode(TRIGGER, OUTPUT);
-  digitalWrite(TRIGGER, LOW);
+	pinMode(TRIGGER, OUTPUT);
+	digitalWrite(TRIGGER, LOW);
 
-  pinMode(ECHO_RIGHT, INPUT);
-  pinMode(ECHO_LEFT, INPUT);
+	pinMode(ECHO_RIGHT, INPUT);
+	pinMode(ECHO_LEFT, INPUT);
 
+	// Utilisation d'une boucle pour optimiser l'initialisation
+	for (int i = 0; i < 8; i++)
+	{
+		pinMode(ROW_PINS[i], OUTPUT);
+		pinMode(COLUMN_PINS[i], OUTPUT);
+	}
 
-  for (int i = 0; i < 8; i++)
-  {
-    pinMode(ROW_PINS[i], OUTPUT);
-    pinMode(COLUMN_PINS[i], OUTPUT);
-  }
+	// Système d'interruption
+	attachInterrupt(digitalPinToInterrupt(ECHO_LEFT), InterruptLeft, CHANGE);
+	attachInterrupt(digitalPinToInterrupt(ECHO_RIGHT), InterruptRight, CHANGE);
 
-  attachInterrupt(digitalPinToInterrupt(ECHO_LEFT), InterruptLeft, CHANGE  );
-  attachInterrupt(digitalPinToInterrupt(ECHO_RIGHT), InterruptRight, CHANGE  );
-
-  startTimerBlink = micros();
-  currentImage = eyeForward;
-  imageState = 0;
+	startTimerBlink = micros();
+	currentImage = eyeForward;
+	imageState = 0;
 }
 
-
+// Appelé à chaque changement de valeur du télémètre gauche
 void InterruptLeft() {
-  if ( digitalRead(ECHO_LEFT) == HIGH)
-  {
-    startTime = micros();
-  }
-  else
-  {
-    lastPulseTimeLeft = (micros() - startTime) / 148;
-  }
+	if (digitalRead(ECHO_LEFT) == HIGH)
+	{
+		// Trigger lancé -> Start Time initialisé
+		startTime = micros();
+	}
+	else
+	{
+		// La distance est stockée pour être utilisée dans switchOnTrigger lors de son appel
+		lastPulseTimeLeft = (micros() - startTime) / 148;
+	}
 }
 
+// Appelé à chaque changement de valeur du télémètre droit
 void InterruptRight() {
-  if ( digitalRead(ECHO_RIGHT) == HIGH)
-  {
-    startTime = micros();
-  }
-  else
-  {
-    lastPulseTimeRight = (micros() - startTime) / 148;
-  }
+	if (digitalRead(ECHO_RIGHT) == HIGH)
+	{
+		// Trigger lancé -> Start Time initialisé
+		startTime = micros();
+	}
+	else
+	{
+		// La distance est stockée pour être utilisée dans switchOnTrigger lors de son appel
+		lastPulseTimeRight = (micros() - startTime) / 148;
+	}
 }
 
 
 // Sous-routine pour la commande des LEDs à partir des données du tableau
 // Elle s'affiche pendant la durée donnée en second paramètre
-void aff(byte * image, unsigned long duree)
+void showImage(byte * image, unsigned long duree)
 {
-  // On note le moment où l'on commence
-  unsigned long debut = millis();
-  // On joue la boucle tant qu'on n'a pas atteint la durée souhaitée
-  while (debut + duree > millis())
-  {
-    // Première itération (boucle for) sur les lignes
-    for (int row = 0; row < 8; row++)
-    {
-      // Connexion de la ligne au +5V
-      digitalWrite(ROW_PINS[row], HIGH);
-      // Seconde itération sur les colonnes
-      for (int col = 0; col < 8; col++)
-      {
-        // Miroir sur le poids du bit pour avoir le bon numéro de colonne
-        int situ = 7 - col;
-        // Lecture du bit concerné
-        boolean pixel = bitRead(image[row], col);
-        // S'il est à un...
-        if (pixel == 1)
-        {
-          // ...on connecte la colonne concernée à la masse pour allumer la LED
-          digitalWrite(COLUMN_PINS[situ], LOW);
-        }
-        // On attend un peu entre chaque LED (délai total ~20ms pour 64 LEDs)
-        delayMicroseconds(300);
-        // Et on déconnecte la colonne de la masse...
-        digitalWrite(COLUMN_PINS[situ], HIGH);
-      }
-      // ...puis la ligne de l'alimentation
-      digitalWrite(ROW_PINS[row], LOW);
-    }
-  }
+	// On note le moment où l'on commence
+	unsigned long debut = millis();
+	// On joue la boucle tant qu'on n'a pas atteint la durée souhaitée
+	while (debut + duree > millis())
+	{
+		// Première itération (boucle for) sur les lignes
+		for (int row = 0; row < 8; row++)
+		{
+			// Connexion de la ligne au +5V
+			digitalWrite(ROW_PINS[row], HIGH);
+			// Seconde itération sur les colonnes
+			for (int col = 0; col < 8; col++)
+			{
+				// Miroir sur le poids du bit pour avoir le bon numéro de colonne
+				int situ = 7 - col;
+				// Lecture du bit concerné
+				boolean pixel = bitRead(image[row], col);
+				// S'il est à un...
+				if (pixel == 1)
+				{
+					// ...on connecte la colonne concernée à la masse pour allumer la LED
+					digitalWrite(COLUMN_PINS[situ], LOW);
+				}
+				// On attend un peu entre chaque LED (délai total ~20ms pour 64 LEDs)
+				delayMicroseconds(300);
+				// Et on déconnecte la colonne de la masse...
+				digitalWrite(COLUMN_PINS[situ], HIGH);
+			}
+			// ...puis la ligne de l'alimentation
+			digitalWrite(ROW_PINS[row], LOW);
+		}
+	}
 }
 
 
 
 void SetImage() {
-  if (  micros() - startTime >= TIME_MEASURE) {
-    if (lastPulseTimeLeft > MAX_DISTANCE && lastPulseTimeRight > MAX_DISTANCE)
-    {
-      imageState = FORWARD_STATE;
-      currentImage = eyeForward;
-    }
-    else if (lastPulseTimeLeft > lastPulseTimeRight)
-    {
-      currentImage = eyeRight;
-      if (imageState != RIGHT_STATE) {
-        currentImage = eyeForward;
-        imageState = RIGHT_STATE;
-      }
-    }
-    else if (lastPulseTimeLeft < lastPulseTimeRight)
-    {
-      currentImage = eyeLeft;
-      if (imageState != LEFT_STATE)
-      {
-        currentImage = eyeForward;
-        imageState = LEFT_STATE;
-      }
-    }
+	if (micros() - startTime >= TIME_MEASURE) {
+		// Condition sur la distance la plus courte droite ou gauche avec un objet.
+		if (lastPulseTimeLeft > MAX_DISTANCE && lastPulseTimeRight > MAX_DISTANCE)
+		{
+			imageState = FORWARD_STATE;
+			currentImage = eyeForward;
+		}
+		else if (lastPulseTimeLeft > lastPulseTimeRight)
+		{
+			currentImage = eyeRight;
+			if (imageState != RIGHT_STATE) {
+				currentImage = eyeForward;
+				imageState = RIGHT_STATE;
+			}
+		}
+		else if (lastPulseTimeLeft < lastPulseTimeRight)
+		{
+			currentImage = eyeLeft;
+			if (imageState != LEFT_STATE)
+			{
+				currentImage = eyeForward;
+				imageState = LEFT_STATE;
+			}
+		}
 
-    digitalWrite(TRIGGER, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGGER, LOW);
+		// Emet l'ultrason
+		digitalWrite(TRIGGER, HIGH);
+		delayMicroseconds(10);
+		digitalWrite(TRIGGER, LOW);
 
-    lastPulseTimeLeft = lastPulseTimeRight = 0;
-  }
+		// Reset
+		lastPulseTimeLeft = lastPulseTimeRight = 0;
+	}
 }
 
 void loop() {
-  if (startTimerBlink + TIME_BLINK < micros())
-  {
-    currentImage = eyeBlink;
-    startTimerBlink = micros();
-    blinked = true;
-  }
+	// Condition pour cligner des yeux
+	if (startTimerBlink + TIME_BLINK < micros())
+	{
+		currentImage = eyeBlink;
+		startTimerBlink = micros();
+		blinked = true;
+	}
 
-  aff(currentImage, 200);
+	showImage(currentImage, 200);
 
-  if (blinked)
-  {
-    currentImage = eyeForward;
-    blinked = false;
-  }
+	// Si on a fermé les yeux -> On les rouvre pour cligner
+	if (blinked)
+	{
+		currentImage = eyeForward;
+		blinked = false;
+	}
 
-  SetImage();
-
+	// On appelle pour savoir quelle image afficher
+	SetImage();
 }
